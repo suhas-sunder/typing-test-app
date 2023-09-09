5728c17e5a3e63ef804186f76b84da920d27f8d6
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-09 17:53:25 -0400

    feat: complete initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
+createBrowserRouter([
+  {
+    path: "/",
+  },
+]);
 
 function App() {
   const [count, setCount] = useState(0);
