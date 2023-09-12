4300276becf9c3284d480a7ec4e0e4d684f36c79
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-12 17:56:21 -0400

    refactor: navigation
    -Refactor links in footer component. -Add copyright text to bottom of footer.

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -1,9 +1,54 @@
-import NavLinks from "./NavLinks";
+import { NavLink } from "react-router-dom";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer className="flex w-full justify-evenly p-5 bg-slate-700 text-white">
-      <NavLinks addClass={"footer-links"} />
+    <footer className="flex flex-col items-center w-full  bg-slate-700 text-white text-center">
+      <div className="flex w-3/4 justify-evenly m-5">
+        <ul>
+          <li>
+            <NavLink to="/" className="flex p-5">
+              Tests
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/lessons" className="flex p-5">
+              Lessons
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/games" className="flex p-5">
+              Games
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/summary" className="flex p-5">
+              Summary
+            </NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to="/login" className="flex p-5">
+              Sign Up Free!
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className="flex p-5">
+              Log In
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings" className="flex p-5">
+              Settings
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="bg-slate-800 w-full p-5">
+        <span className="">&copy;</span> 2023 | FreeTypingCamp
+      </div>
     </footer>
   );
 }
