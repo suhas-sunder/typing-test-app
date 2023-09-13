10fcce3c05e6330e53edd4820f44462410117e21
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-12 22:15:28 -0400

    refactor: typing interface
    -Add new component called TypingWindow that manages TextBox  and TypingStats. -Move state from TextBox component to TypingWindow component. -Move dummyText to TypingWindow component. -Update character input validity in TypingWindow component instead of TextBox component.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,16 @@
-import TypingInterface from "../components/layout/TextBox";
+
 import HeaderDashboard from "../components/layout/HeaderDashboard";
 import Leaderboard from "./Leaderboard";
 import AchievementSummary from "../components/layout/AchievementSummary";
 import Button from "../components/ui/Button";
+import TypingWindow from "../components/layout/TypingWindow";
 
 function Home() {
   return (
     <div className="flex flex-col items-center w-full">
       {/* Don't display header on mobile. Move textbox to the top of the screen. */}
       <HeaderDashboard />
-      <TypingInterface />
+      <TypingWindow />
       <div className="flex w-full max-w-7xl justify-evenly items-center">
         {/* Buttons for Train your skills and Games. Also use grid instead of flex to align items.*/}
         <div>
