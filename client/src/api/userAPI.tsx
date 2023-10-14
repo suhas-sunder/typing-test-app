2a4d50f84a8577932ed8af03fc4a6e9ff06d469f
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-10-13 21:49:27 -0400

    fix: prep url for deployment

--- client/src/api/userAPI.tsx
+++ client/src/api/userAPI.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "/v1/api/user/"
-    : "http://localhost:3100/v1/api/user/";
+const baseURL = "/v1/api/user/";
+// process.env.NODE_ENV === "production"
+//   ? "/v1/api/user/"
+//   : "http://localhost:3001/v1/api/user/";
 
 const instance = axios.create({
   baseURL: baseURL,
