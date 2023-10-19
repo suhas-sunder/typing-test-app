4f73d0858e64f734341c429cdf3d6b862bfd4f83
suhas-sunder <suhas@live.ca>
2023-10-19 00:50:04 -0400

    fix: char validity tracking
    -Add empty array of strings based on test length when charIsValid is reset to fix bug that doesn't track char validity.

--- client/src/components/layout/MainMenu.tsx
+++ client/src/components/layout/MainMenu.tsx
@@ -9,12 +9,13 @@ function MainMenu() {
   const [showGameOverMenu, setShowGameOverMenu] = useState<boolean>(false);
   const [startTimer, setStartTimer] = useState<boolean>(false);
   const [testTimeSeconds, setTestTimeSeconds] = useState(60);
-  const [cursorPosition, setCursorPosition] = useState(0);
+  const [cursorPosition, setCursorPosition] = useState(0); //Keeps track of cursor position while typing
   const [firstInputDetected, setFirstInputDetected] = useState<boolean>(false); //Used to track if test started
   const [text, setText] = useState<string>("asdf");
 
-  const [charIsValid, setCharIsValid] = useState<string[]>([""]);
+  const [charIsValid, setCharIsValid] = useState<string[]>([""]); //Tracks every character input as valid or invalid
 
+  // Updates character input validity **Need to rename this function**
   const handleStateChange = (cursorIndex: number, newValue: string) => {
     setCharIsValid(
       charIsValid.map((charStatus, index) =>
@@ -23,24 +24,22 @@ function MainMenu() {
     );
   };
 
+  // Reset states for end test
   const handleEndTest = useCallback(() => {
-    console.log("Test ended");
     setShowGameOverMenu(true);
     setStartTimer(false);
   }, []);
 
+  // For clearing all data when test is restarted or ended
   const clearTestData = () => {
-    setCharIsValid([""]);
+    setCharIsValid(new Array(text.length).fill(""));
     setShowGameOverMenu(false);
     setCursorPosition(0);
     setFirstInputDetected(false);
     setStartTimer(false);
   };
 
-  const handleRestartTest = () => {
-    clearTestData();
-  };
-
+  // Reset states for main menu
   const handleReturnToMenu = () => {
     setStartTest(false);
     clearTestData();
@@ -64,7 +63,7 @@ function MainMenu() {
           endTest={handleEndTest}
           testTime={testTimeSeconds}
           firstInputDetected={firstInputDetected}
-          handleRestart={handleRestartTest}
+          handleRestart={clearTestData}
           showMainMenu={handleReturnToMenu}
         />
       )}
@@ -81,16 +80,19 @@ function MainMenu() {
         />
       )}
 
+      {/* I may just make this the same for both game over menu and test menu for simplicity. Overcomplicated this. */}
       {!showGameOverMenu && startTest && (
         <div>
           <button type="button" onClick={handleReturnToMenu}>
             Main Menu
           </button>
-          <button type="button" onClick={handleRestartTest}>
+          <button type="button" onClick={clearTestData}>
             Restart
           </button>
         </div>
       )}
+
+      {/* Feature to be added in the future */}
       <label className="justify-center m-auto border-2 border-slate-200 rounded-md p-2 w-40 hidden">
         Show Keyboard (Make this a toggle setting top right.) Hide/Show stats
         <input type="checkbox" className="hidden" />
