ae5742f5e09e0ac04e7c6b7bcb4d1991fb70ed6f
suhas-sunder <suhas@live.ca>
2023-10-27 15:04:00 -0400

    refactor: test for login links.

--- client/src/components/ui/navigation/__tests__/LoginLinks.test.tsx
+++ client/src/components/ui/navigation/__tests__/LoginLinks.test.tsx
@@ -1,7 +1,7 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import LoginLinks from "../LoginLinks";
 
 interface PropTypes {
@@ -10,41 +10,41 @@ interface PropTypes {
 }
 const MockLoginLinks = ({ showMobileMenu, setShowMobileMenu }: PropTypes) => {
   render(
-    <BrowserRouter>
+    <MemoryRouter>
       <LoginLinks
         showMobileMenu={showMobileMenu}
         setShowMobileMenu={setShowMobileMenu}
       />
-    </BrowserRouter>
+    </MemoryRouter>
   );
 };
 
 const setShowMobileMenu = vi.fn();
 const showMobileMenu = false;
 
+beforeEach(() => {
+  MockLoginLinks({ showMobileMenu, setShowMobileMenu });
+});
+
 describe("renders all elements", () => {
   it("should render a login link", () => {
-    MockLoginLinks({ showMobileMenu, setShowMobileMenu });
     const linkElement = screen.getByRole("link", { name: /login/i });
     expect(linkElement).toBeInTheDocument();
   });
 
   it("should render a register link", () => {
-    MockLoginLinks({ showMobileMenu, setShowMobileMenu });
     const linkElement = screen.getByRole("link", { name: /sign up/i });
     expect(linkElement).toBeInTheDocument();
   });
 
   it("should render two list items", () => {
-    MockLoginLinks({ showMobileMenu, setShowMobileMenu });
-    const linkElements = screen.getAllByRole("listitem");
-    expect(linkElements).toHaveLength(2);
+    const listElements = screen.getAllByRole("listitem");
+    expect(listElements).toHaveLength(2);
   });
 });
 
 describe("function props are called as intended", () => {
   it("should close mobile menu when login link is clicked", () => {
-    MockLoginLinks({ showMobileMenu, setShowMobileMenu });
     const linkElement = screen.getByRole("link", { name: /login/i });
 
     fireEvent.click(linkElement);
@@ -52,7 +52,6 @@ describe("function props are called as intended", () => {
   });
 
   it("should close mobile menu when register link is clicked", () => {
-    MockLoginLinks({ showMobileMenu, setShowMobileMenu });
     const linkElement = screen.getByRole("link", { name: /sign up/i });
 
     fireEvent.click(linkElement);
