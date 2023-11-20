8249ce2613410b40a509e525a1bc7e085c37b590
suhas-sunder <suhas@live.ca>
2023-11-20 14:35:08 -0500

    refactor: nav auth
    -Use context to manage auth state for nav bar instead of prop drilling.

--- client/src/components/navigation/LogoutBtn.tsx
+++ client/src/components/navigation/LogoutBtn.tsx
@@ -1,17 +1,19 @@
 import styles from "./styles/NavBar.module.css";
 import Icon from "../../utils/Icon";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useContext } from "react";
 
 interface PropType {
   customStyle: string;
-  setAuth: (value: boolean) => void;
 }
 
-function LogoutBtn({ setAuth, customStyle }: PropType) {
+function LogoutBtn({ customStyle }: PropType) {
+  const { setIsAuthenticated } = useContext(AuthContext);
   // Clear local storage
   const handleLogout = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     localStorage.removeItem("jwt_token");
-    setAuth(false);
+    setIsAuthenticated(false);
   };
 
   return (
