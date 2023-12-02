934352cc4bb32bda47fe5bc0d87da72334e60aa6
suhas-sunder <suhas@live.ca>
2023-12-01 23:44:24 -0500

    feat: sitemap.xml & robots.txt

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression() ],
+  plugins: [
+    react(), 
+    compression(),  
+],
   test: {
     globals: true,
     environment: 'jsdom',
