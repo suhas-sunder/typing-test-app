906ac9b5a548d42ce916f445bf3ca97d91c11340
suhas-sunder <suhas@live.ca>
2023-12-09 16:02:35 -0500

    feat: webp image format

--- client/src/components/navigation/ImgLinks.tsx
+++ client/src/components/navigation/ImgLinks.tsx
@@ -7,6 +7,7 @@ import SparkleAnim from "../../utils/SparkleAnim";
 interface PropType {
   linkData: {
     img: { [key: string]: string };
+    webpImgSrc: string;
     link: string;
     text: string;
   }[];
@@ -15,7 +16,9 @@ interface PropType {
 
 function ImgLinks({ linkData, customStyle }: PropType) {
   return (
-    <ul className={`${customStyle} grid max-w-[1060px] grid-cols-1 items-center justify-center gap-y-28 font-lato capitalize text-gray-500 sm:grid-cols-2 sm:gap-x-14 sm:gap-y-20 md:mx-8 md:grid-cols-3 md:gap-x-16 md:gap-y-24  lg:gap-16`}>
+    <ul
+      className={`${customStyle} grid max-w-[1060px] grid-cols-1 items-center justify-center gap-y-28 font-lato capitalize text-gray-500 sm:grid-cols-2 sm:gap-x-14 sm:gap-y-20 md:mx-8 md:grid-cols-3 md:gap-x-16 md:gap-y-24  lg:gap-16`}
+    >
       {/* <Icon icon="sparkleFill" title="sparkle fill icon" customStyle="" /> Add floating sparkles on hover */}
       {linkData.map((data) => (
         <li key={uuidv4()} className="hover:scale-[1.03]">
@@ -24,13 +27,16 @@ function ImgLinks({ linkData, customStyle }: PropType) {
               to={data.link}
               className="z-[10] flex max-w-[15em] flex-col items-center justify-center rounded-lg border-2 bg-transparent bg-white px-5 py-6 pb-10  hover:border-sky-400 hover:text-sky-700"
             >
-              <img
-                {...data.img}
-                loading="lazy"
-                className="flex rounded-md"
-                width={480}
-                height={784}
-              />
+              <picture>
+                <source srcSet={data.webpImgSrc} type="image/webp"></source>
+                <img
+                  {...data.img}
+                  loading="lazy"
+                  className="flex rounded-md"
+                  width={480}
+                  height={784}
+                />
+              </picture>
               <span className="absolute -bottom-4 flex items-center justify-center rounded-full border-2 bg-white px-4 py-2 text-sm tracking-wider md:text-[0.9rem]">
                 {data.text.toString()}
               </span>
--- client/src/components/ui/ProfileImageLink.tsx
+++ client/src/components/ui/ProfileImageLink.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import SparkleAnim from "../../utils/SparkleAnim";
 import ProfileImg from "../../assets/images/wolf_icon.jpg";
+import ProfileImgWebp from "../../assets/images/wolf_icon.webp";
 
 function ProfileImageLink() {
   return (
@@ -10,13 +11,16 @@ function ProfileImageLink() {
         title="Profile page"
         className="relative flex max-w-[9em] cursor-pointer justify-center rounded-lg bg-slate-800 hover:scale-105"
       >
-        <img
-          src={ProfileImg}
-          alt="Colourful wolf standing on a mountain top."
-          className={`relative flex w-full rounded-lg border-4 border-slate-800 object-cover pb-[0.5em] drop-shadow-lg`}
-          width={480}
-          height={784}
-        />
+        <picture>
+          <source srcSet={ProfileImgWebp} type="image/webp"></source>
+          <img
+            src={ProfileImg}
+            alt="Colourful wolf standing on a mountain top."
+            className={`relative flex w-full rounded-lg border-4 border-slate-800 object-cover pb-[0.5em] drop-shadow-lg`}
+            width={480}
+            height={784}
+          />
+        </picture>
         <p className="absolute bottom-0 w-full rounded-full bg-slate-800 px-3 py-[0.3em] text-center text-[0.75rem] tracking-wider">
           Level: 0
         </p>
