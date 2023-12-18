640a617be77d0620e74e379ef0015b1a9aed2a47
suhas-sunder <suhas@live.ca>
2023-12-18 02:28:26 -0500

    style: login & register
    -Add styling to login and register page. -Change password input type to password instead of text.

--- client/src/components/forms/LoginForm.tsx
+++ client/src/components/forms/LoginForm.tsx
@@ -18,7 +18,7 @@ function LoginForm({
   return (
     <form
       onSubmit={submitForm}
-      className="relative ml-10 mr-10 flex w-full max-w-md flex-col gap-4 text-xl"
+      className="relative mx-5 flex w-full  max-w-md flex-col gap-4 font-nunito text-xl"
     >
       {formData.map((data) => (
         <LoginFormInputs
@@ -29,13 +29,50 @@ function LoginForm({
         />
       ))}
       {Object.prototype.hasOwnProperty.call(inputValues, "emailOrUsername") && (
-        <div className="relative flex justify-between pl-6 pr-6">
+        <div className="relative mt-2 flex justify-between text-sm sm:text-base">
           {/* Add a remember-me and forgot password option here */}
-          <Link to="/register">register</Link>
-          <Link to="/register">forgot password</Link>
+          <div className="flex gap-2">
+            <input
+              id="remember-me-input"
+              type="checkbox"
+              className="cursor-pointer"
+            />
+            <label htmlFor="remember-me-input" className="cursor-pointer">
+              Remember me
+            </label>
+          </div>
+          <div className="underline underline-offset-2">
+            <Link to="/register" className="cursor-pointer">
+              Forgot your password?
+            </Link>
+          </div>
         </div>
       )}
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="text-md mt-3 flex w-full items-center justify-center rounded-lg border-2 bg-sky-700 py-4 text-white outline-green-900 hover:scale-[1.01] hover:brightness-105"
+      >
+        {Object.prototype.hasOwnProperty.call(inputValues, "emailOrUsername")
+          ? "Login"
+          : "Sign Up"}
+      </button>
+
+      <div className="relative mt-1 flex justify-center text-center text-base sm:text-lg ">
+        {Object.prototype.hasOwnProperty.call(
+          inputValues,
+          "emailOrUsername",
+        ) ? (
+          <Link to="/register">
+            Don't have an account?{" "}
+            <span className="underline underline-offset-2">Sign Up Now!</span>
+          </Link>
+        ) : (
+          <Link to="/login">
+            Already have an account?{" "}
+            <span className="underline underline-offset-2">Login Now!</span>
+          </Link>
+        )}
+      </div>
     </form>
   );
 }
