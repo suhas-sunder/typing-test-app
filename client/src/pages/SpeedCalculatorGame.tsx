0a275a3f0ebca36db84bd913c7491ae7f82e4a9d
suhas-sunder <suhas@live.ca>
2024-05-21 18:55:05 -0400

    fix: build errors

--- client/src/pages/SpeedCalculatorGame.tsx
+++ client/src/pages/SpeedCalculatorGame.tsx
@@ -359,7 +359,6 @@ function SpeedCalculatorGame() {
                 inputValidity={inputValidity}
                 cursorPosition={cursorPosition}
                 calculations={calculations}
-                startGame={startGame}
                 validInputKeys={validInputKeys}
               />
             </div>
@@ -370,6 +369,7 @@ function SpeedCalculatorGame() {
             handleDifficulty={handleDifficulty}
             startGame={startGame}
             anchorURL={"/speed-calculator#difficulty-faq"}
+            setDifficultyLevel={setDifficultyLevel}
           />
         )}
         <div className="mt-2 flex w-full items-center justify-center gap-10">
