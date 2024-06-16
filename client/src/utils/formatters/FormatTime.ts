f308f175bb0e31f3b66534cd8477e0f78c71d075
suhas-sunder <suhas@live.ca>
2024-06-16 19:28:27 -0400

    test: formatters
    -Add relevant unit tests to all utility formatter components to ensure all data is formatted correctly.

--- client/src/utils/formatters/FormatTime.ts
+++ client/src/utils/formatters/FormatTime.ts
@@ -1,23 +1,25 @@
-function FormatTime(totalTimeSec) {
-  const hours = Math.floor(totalTimeSec / 3600).toLocaleString("en-US", {
+function FormatTime(totalTimeSec: number) {
+  if (totalTimeSec < 0) console.error("Total time  should not be negative!");
+  const hours = Math.abs(Math.floor(totalTimeSec / 3600)).toLocaleString(
+    "en-US",
+    {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    },
+  );
+
+  const minutes = Math.abs(
+    Math.floor((totalTimeSec % 3600) / 60),
+  ).toLocaleString("en-US", {
     minimumIntegerDigits: 2,
     useGrouping: false,
   });
-
-  const minutes = Math.floor((totalTimeSec % 3600) / 60).toLocaleString(
-    "en-US",
-    {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    },
-  );
-  const seconds = Math.floor((totalTimeSec % 3600) % 60).toLocaleString(
-    "en-US",
-    {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    },
-  );
+  const seconds = Math.abs(
+    Math.floor((totalTimeSec % 3600) % 60),
+  ).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
 
   return { hours, minutes, seconds };
 }
