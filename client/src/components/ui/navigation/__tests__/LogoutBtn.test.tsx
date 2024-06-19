7c0b09144cd1f9df4910a7d9897699e39100f491
suhas-sunder <suhas@live.ca>
2024-06-19 01:12:26 -0400

    test: navigation
    -Add test to logout button. -Delete tests for navigation components that don't exist anymore.

--- client/src/components/ui/navigation/__tests__/LogoutBtn.test.tsx
+++ client/src/components/ui/navigation/__tests__/LogoutBtn.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import { MemoryRouter } from "react-router-dom";
 import LogoutBtn from "../LogoutBtn";
@@ -20,6 +20,17 @@ const customStyle = "";
 
 beforeEach(() => {
   MockLoginLinks({ customStyle });
+  // Mock localStorage
+  Object.defineProperty(window, "localStorage", {
+    value: {
+      removeItem: vi.fn(),
+    },
+    writable: true,
+  });
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
 });
 
 describe("renders all elements", () => {
@@ -30,8 +41,16 @@ describe("renders all elements", () => {
     expect(btnElement).toBeInTheDocument();
   });
 
-  it("should render a an icon for the button", async () => {
+  it("should render an icon for the button", async () => {
     const iconElement = await screen.findByTitle(/icon/i);
     expect(iconElement).toBeInTheDocument();
+  });
+});
+
+describe("handles user events correctly", () => {
+  it("should trigger handleLogout function when clicked", () => {
+    const buttonElement = screen.getByRole("button", { name: /logout/i });
+    fireEvent.click(buttonElement);
+    expect(window.localStorage.removeItem).toHaveBeenCalledWith("jwt_token"); // assuming 'jwt_token' is the item being removed
   });
 });
