388eb1ce2301aa321ced57248a675ec1fa2b7f29
Suhas Sunder <suhas@live.ca>
2024-12-15 15:42:19 -0500

    refactor: registration pg
    -Disable registration for now until nodemailer issues can be resolved.

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,170 +1,176 @@
-//Register
+// //Register
+// import { useState } from "react";
+// import ServerAPI from "../api/userAPI";
 
-import { useState } from "react";
-import ServerAPI from "../api/userAPI";
-
-import PasswordValidation from "../utils/validation/PasswordValidation";
-import type { AuthFormData } from "./Login";
-import {
-  RegExpMatcher,
-  englishDataset,
-  englishRecommendedTransformers,
-} from "obscenity";
-import SendEmailVerification from "../components/forms/shared/SendEmailVerification";
-import LoginForm from "../components/forms/shared/LoginForm";
+// import PasswordValidation from "../utils/validation/PasswordValidation";
+// import type { AuthFormData } from "./Login";
+// import {
+//   RegExpMatcher,
+//   englishDataset,
+//   englishRecommendedTransformers,
+// } from "obscenity";
+// import SendEmailVerification from "../components/forms/shared/SendEmailVerification";
+// import LoginForm from "../components/forms/shared/LoginForm";
 
 function Register() {
-  const [verifyEmailMsg, setVerifyEmailMsg] = useState<boolean>(false);
-  const [inputValues, setInputValues] = useState<{ [key: string]: string }>({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  // const [verifyEmailMsg, setVerifyEmailMsg] = useState<boolean>(false);
+  // const [inputValues, setInputValues] = useState<{ [key: string]: string }>({
+  //   username: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // });
 
-  const registerData: AuthFormData = [
-    {
-      id: "username",
-      name: "username",
-      type: "text",
-      placeholder: "Username",
-      label: "Username",
-      pattern: "^.{6,16}$",
-      err: "Username must be between 6 and 16 characters!",
-      required: true,
-      asterisk: true,
-    },
-    {
-      id: "email",
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      label: "Email",
-      // eslint-disable-next-line no-useless-escape
-      pattern: "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/",
-      err: "Please enter a valid email!",
-      required: true,
-      asterisk: true,
-    },
-    {
-      id: "password",
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      label: "Password",
-      required: true,
-      asterisk: true,
-    },
-    {
-      id: "confirm-password",
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      label: "Confirm Password",
-      pattern: "",
-      err: "Password does not match!",
-      required: true,
-      asterisk: true,
-    },
-  ];
-  //profanity matcher to check for profane usernames
-  const matcher = new RegExpMatcher({
-    ...englishDataset.build(),
-    ...englishRecommendedTransformers,
-  });
+  // const registerData: AuthFormData = [
+  //   {
+  //     id: "username",
+  //     name: "username",
+  //     type: "text",
+  //     placeholder: "Username",
+  //     label: "Username",
+  //     pattern: "^.{6,16}$",
+  //     err: "Username must be between 6 and 16 characters!",
+  //     required: true,
+  //     asterisk: true,
+  //   },
+  //   {
+  //     id: "email",
+  //     name: "email",
+  //     type: "email",
+  //     placeholder: "Email",
+  //     label: "Email",
+  //     // eslint-disable-next-line no-useless-escape
+  //     pattern: "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/",
+  //     err: "Please enter a valid email!",
+  //     required: true,
+  //     asterisk: true,
+  //   },
+  //   {
+  //     id: "password",
+  //     name: "password",
+  //     type: "password",
+  //     placeholder: "Password",
+  //     label: "Password",
+  //     required: true,
+  //     asterisk: true,
+  //   },
+  //   {
+  //     id: "confirm-password",
+  //     name: "confirmPassword",
+  //     type: "password",
+  //     placeholder: "Confirm Password",
+  //     label: "Confirm Password",
+  //     pattern: "",
+  //     err: "Password does not match!",
+  //     required: true,
+  //     asterisk: true,
+  //   },
+  // ];
+  // //profanity matcher to check for profane usernames
+  // const matcher = new RegExpMatcher({
+  //   ...englishDataset.build(),
+  //   ...englishRecommendedTransformers,
+  // });
 
-  const [serverError, setServerError] = useState<string>("");
+  // const [serverError, setServerError] = useState<string>("");
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    const privacyTos = formData.get("privacy-tos");
+  //   const formData = new FormData(e.currentTarget);
+  //   const privacyTos = formData.get("privacy-tos");
 
-    if (!privacyTos) {
-      setServerError(
-        `Users must read and accept terms and conditions before creating an account!`,
-      );
-      return;
-    }
+  //   if (!privacyTos) {
+  //     setServerError(
+  //       `Users must read and accept terms and conditions before creating an account!`,
+  //     );
+  //     return;
+  //   }
 
-    if (matcher.hasMatch(inputValues.username)) {
-      setServerError(
-        `Sorry, your account was not created. The system has detected profanity in your username "${inputValues.username}" which is against FreeCodeCamp's username policy. Contact us at admin@freecodecamp.com if this is a mistake.`,
-      );
-      return;
-    }
+  //   if (matcher.hasMatch(inputValues.username)) {
+  //     setServerError(
+  //       `Sorry, your account was not created. The system has detected profanity in your username "${inputValues.username}" which is against FreeCodeCamp's username policy. Contact us at admin@freecodecamp.com if this is a mistake.`,
+  //     );
+  //     return;
+  //   }
 
-    const passwordValidationErr = PasswordValidation({
-      password: inputValues.password,
-    });
+  //   const passwordValidationErr = PasswordValidation({
+  //     password: inputValues.password,
+  //   });
 
-    //Display validation error and skip submission
-    if (passwordValidationErr) {
-      setServerError(passwordValidationErr);
-      return;
-    }
+  //   //Display validation error and skip submission
+  //   if (passwordValidationErr) {
+  //     setServerError(passwordValidationErr);
+  //     return;
+  //   }
 
-    try {
-      const data = {
-        firstName: inputValues.firstName,
-        lastName: inputValues.lastName,
-        username: inputValues.username,
-        email: inputValues.email,
-        password: inputValues.password,
-      };
+  //   try {
+  //     const data = {
+  //       firstName: inputValues.firstName,
+  //       lastName: inputValues.lastName,
+  //       username: inputValues.username,
+  //       email: inputValues.email,
+  //       password: inputValues.password,
+  //     };
 
-      const response = await ServerAPI.post("/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        data: data,
-      })
-        .then((response) => {
-          return response.data;
-        })
-        .catch((err) => {
-          let message;
+  //     const response = await ServerAPI.post("/register", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       data: data,
+  //     })
+  //       .then((response) => {
+  //         return response.data;
+  //       })
+  //       .catch((err) => {
+  //         let message;
 
-          if (err instanceof Error) {
-            message = err.message;
-          } else {
-            message = String(err);
-          }
+  //         if (err instanceof Error) {
+  //           message = err.message;
+  //         } else {
+  //           message = String(err);
+  //         }
 
-          message.includes("Network") &&
-            setServerError(
-              "500 Internal Server Error. Please try again later!",
-            );
+  //         message.includes("Network") &&
+  //           setServerError(
+  //             "500 Internal Server Error. Please try again later!",
+  //           );
 
-          message.includes("401") &&
-            setServerError("An account with this email already exists!");
+  //         message.includes("401") &&
+  //           setServerError("An account with this email already exists!");
 
-          console.log(message);
-        });
+  //         console.log(message);
+  //       });
 
-      const parseRes = await response;
+  //     const parseRes = await response;
 
-      if (parseRes) {
-        setVerifyEmailMsg(true);
-      } else {
-        console.log("Error creating creating user account");
-      }
-    } catch (err) {
-      let message;
+  //     if (parseRes) {
+  //       setVerifyEmailMsg(true);
+  //     } else {
+  //       console.log("Error creating creating user account");
+  //     }
+  //   } catch (err) {
+  //     let message;
 
-      if (err instanceof Error) {
-        message = err.message;
-      } else {
-        message = String(err);
-      }
+  //     if (err instanceof Error) {
+  //       message = err.message;
+  //     } else {
+  //       message = String(err);
+  //     }
 
-      console.error(message);
-    }
-  };
+  //     console.error(message);
+  //   }
+  // };
 
   return (
     <div className="relative mt-12 flex flex-col items-center gap-12 px-5">
-      <header>
+      <h1 className="text-center font-nunito text-3xl text-defaultblue mx-10 max-w-[40em] leading-loose">
+        {" "}
+        New user registration is currently at capacity and is undergoing maintenance. If
+        you wish to join the waitlist, please contact us at
+        freetypingcamp@gmail.com and we'll get back to you as soon as
+        registration re-opens.
+      </h1>
+      {/* <header>
         <h1 className="text-center font-nunito text-3xl text-defaultblue md:text-4xl">
           {verifyEmailMsg ? (
             <span>Email Verification Required!</span>
@@ -190,7 +196,7 @@ function Register() {
             serverError={serverError}
           />
         )}
-      </main>
+      </main> */}
     </div>
   );
 }
